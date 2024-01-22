ede6558084eeea342940572357d7a68f6cffb68c
Jean Pedro <jeanjesuspedrobook@gmail.com>
2024-01-22 01:21:36 -0300

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,46 @@
-import { adcTarefas } from "./addTarefas.js";
+let valorInput = document.getElementById("inputTarefa");
+let valorSelect = document.getElementById("tipoMetaInput");
+let botaoAdc = document.getElementById("btnAdc");
+let warnerSpan = document.getElementById("warnerSpan");
+let contador = 0;
 
-adcTarefas();
+botaoAdc.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(valorInput.value + " " + valorSelect.value);
+  ++contador;
+
+  if (valorSelect.value == "tipo") {
+    warnerSpan.innerHTML = "Escolha um tipo de Meta!";
+  } else if (valorInput.value == "") {
+    warnerSpan.innerHTML = "Escreva uma Meta!";
+  } else {
+    warnerSpan.innerHTML = "";
+
+    let novoItem = `<div class="tarefaBox" id="t${contador}">
+          <span class="material-symbols-outlined" class="checkIcon" id="icon${contador}" onclick="marcarTarefa(${contador})">
+              radio_button_unchecked
+          </span>
+          <p class="nomeTarefa" onclick="marcarTarefa(${contador})">${valorInput.value}</p>
+          <button id="deleteBtn${contador}" onclick="deletar(${contador})">
+              <span class="material-symbols-outlined deleteIcon">
+              delete
+              </span>
+          </button>
+      </div>`;
+
+    function containerTarefas(idContainer) {
+      let containerTaf = document.getElementById(`${idContainer}`);
+      return containerTaf;
+    }
+
+    containerTarefas(valorSelect.value).innerHTML += novoItem;
+    valorInput.value = "";
+    valorInput.focus();
+  }
+});
+
+function deletar(id) {
+  var tarefa = document.getElementById(`t${id}`);
+  tarefa.remove();
+  console.log(tarefa);
+}
